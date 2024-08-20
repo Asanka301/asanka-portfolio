e547520519ff51f4c0446a8bce08fbdfc43fb672
asgconnect <asanka@gconnect.com.au>
2024-08-20 08:27:46 +0530

    edited

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -21,7 +21,7 @@ const Projects = ({ openModal, setOpenModal }) => {
         <Title>Projects</Title>
         <Desc>
           I have worked on a wide range of projects. From web apps to android
-          apps. Here are some of my projects.
+          apps. Here are some of my projects
         </Desc>
         <ToggleButtonGroup>
           {toggle === "all" ? (
